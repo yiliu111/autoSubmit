679088a762d07549a49956f1665eaffff06114f4
yiliu111 <75347050+yiliu111@users.noreply.github.com>
2020-12-02 10:11:05 +0800

    Update main.go

--- main.go
+++ main.go
@@ -23,8 +23,8 @@ var lxdh string
 
 var username = flag.String("username", "", "学号")
 var password = flag.String("password", "", "portal密码")
-var reason = flag.String("reason", "西市买鞍鞯", "出入校事由")
-var track = flag.String("track", "北大西门-畅春园-北大西门", "出校行动轨迹")
+var reason = flag.String("reason", "取快递", "出入校事由")
+var track = flag.String("track", "畅春新园", "出校行动轨迹")
 var ftKey string
 
 func errorNotifier(title string) {
